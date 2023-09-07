714d1fef5d91bea6d465fe9275b32d67bbca5816
kimsoohwan <ubsno4012@gmail.com>
2023-09-07 11:21:28 +0900

    kakaologin 개인정도 가져오기

--- src/app/kakaoRedirect/page.tsx
+++ src/app/kakaoRedirect/page.tsx
@@ -1,60 +1,64 @@
 'use client'
 import axios from "axios"
 import { error } from "console"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const KakaoCallback = () => {
+    const [userInfo, setUserInfo] = useState({
+        nickname: ""
+    })
     useEffect(() => {
-        const params= new URL(document.location.toString()).searchParams;
-        const code = params.get('code');
+        
+        const params= new URL(document.location.toString()).searchParams
+        const code = params.get('code')
 
-        const REST_API_KEY = '7e7b63658cacaa73e2a412dcd713bb8a'
-        const grantType = 'authorization_code'
-        const REDIRECT_URI = 'http://localhost:3000/kakao'
-
-        // axios.post(
-        //     `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${code}`,
-        //      {},
-        //      { headers: { "Content-type": "application/x-www-form-urlencoded;charset=utf-8" } }
-        //     ).then((res: any) => {
-        //         console.log(res)
-        //         const { access_token } = res.data;
-        //     }
-        // )
- 
         const param: any = {
             grant_type: 'authorization_code',
             client_id: '7e7b63658cacaa73e2a412dcd713bb8a',
-            redirect_uri: 'http://localhost:3000/kakao',
+            redirect_uri: 'http://localhost:3000/kakaoRedirect',
             code: code,
         }
         // console.log('param ::', param)
-
         const config = {
             headers: {
-              'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+                'Authorization': 'Bearer ',
+                'Content-type': 'application/x-www-form-urlencodedcharset=utf-8'
             }
         }
         // console.log('config ::', config)
-
         const queryString = Object.keys(param).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(param[key])).join('&')
-        console.log('queryString ::', queryString+ ', {}, { headers: { "Content-type": "application/x-www-form-urlencoded;charset=utf-8" } }')
-        console.log('test ::', queryString, {}, config)
+        console.log('queryString ::', queryString+ ', {}, { headers: { "Content-type": "application/x-www-form-urlencodedcharset=utf-8" } }')
+        console.log('test ::', queryString, config)
         // access_token 발급받기
-        axios.post('https://kauth.kakao.com/oauth/token?' + queryString, {}, config).then()
-              
-        
-        /**
-         * access_token 발급을 받으려고 값을 보내는데 보낼때의 형태를 잘 모르겠습니다
-         * 
-         */
+        axios.post('https://kauth.kakao.com/oauth/token?' + queryString, config)
+        .then((response : any) => {
+            console.log(response.data)
+            config.headers.Authorization += response.data.access_token
+            console.log('config :: ',config)
+          
+            axios.post('https://kapi.kakao.com/v2/user/me', {}, config)
+            .then((res) => {
+                console.log('개인 정보 확인 ', res.data.properties.nickname)
+                const nickname = res.data.properties.nickname
+                setUserInfo({nickname})
+                console.log('userinfo ::', userInfo)
+            })
+        })
+        .catch(error => {
+            console.error(error)
+        })
+        console.log()
         
     }, [])
-    
+    // userInfo 출력
+    useEffect(() => {
+        console.log('userinfo ::', userInfo)
+    }, [])
     return(
         <>
-        <div>Kakao Redirect Page</div>
+        <div>로그인 되었습니다. {userInfo.nickname} 님</div>
+        
         </>
     )
 }
-export default KakaoCallback;
+export default KakaoCallback
